d5a98f44c2b94376448a76eb18a62036ba8202c4
G. R. Achinthani <galagamagerandika@gmail.com>
2024-11-21 23:49:54 +0530

    add components

---
+++ components/footer.php
@@ -0,0 +1,36 @@
+<!-- footer section starts  -->
+
+<footer class="footer">
+
+   <section class="flex">
+
+      <div class="box">
+         <a href="tel:071 2525200"><i class="fas fa-phone"></i><span>071 2525200</span></a>
+         <a href="tel:011 2202200"><i class="fas fa-phone"></i><span>011 2202200</span></a>
+         <a href="mailto:link2serve@gmail.com"><i class="fas fa-envelope"></i><span>link2serve@gmail.com</span></a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i><span>Colombo 11, Sri Lanka - 400104</span></a>
+      </div>
+
+      <div class="box">
+         <a href="Main.php"><span>Home</span></a>
+         <a href="about.php"><span>About</span></a>
+         <a href="contact.php"><span>Contact</span></a>
+         <a href="listings.php"><span>All listings</span></a>
+         <a href="saved.php"><span>Saved properties</span></a>
+      </div>
+
+      <div class="box">
+         <a href="https://www.facebook.com/profile.php?id=61559416891389"><span>Facebook</span><i class="fab fa-facebook-f"></i></a>
+         <a href="#"><span>Twitter</span><i class="fab fa-twitter"></i></a>
+         <a href="#"><span>Linkedin</span><i class="fab fa-linkedin"></i></a>
+         <a href="#"><span>Instagram</span><i class="fab fa-instagram"></i></a>
+
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Waya Group 08</span> | all rights reserved!</div>
+
+</footer>
+
+<!-- footer section ends -->
